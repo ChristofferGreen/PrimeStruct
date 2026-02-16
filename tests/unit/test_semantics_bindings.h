d3db9b0b3416186a202899d1df6cc909ce43138a
Christoffer Green <christoffer.green@arm.com>
2026-02-16 03:16:24 +0100

    Test brace constructor return
    Ensure brace constructors accept return(value) blocks
    for struct arguments.

--- tests/unit/test_semantics_bindings.h
+++ tests/unit/test_semantics_bindings.h
@@ -903,6 +903,30 @@ main() {
   CHECK(error.empty());
 }
 
+TEST_CASE("struct brace constructor accepts return value blocks") {
+  const std::string source = R"(
+[struct]
+thing() {
+  [i32] value{1i32}
+}
+
+[return<int>]
+use([thing] item) {
+  return(1i32)
+}
+
+[return<int>]
+main() {
+  return(use(thing{
+    return(2i32)
+  }))
+}
+)";
+  std::string error;
+  CHECK(validateProgram(source, "/main", error));
+  CHECK(error.empty());
+}
+
 TEST_CASE("binding initializer accepts labeled arguments") {
   const std::string source = R"(
 [struct]
