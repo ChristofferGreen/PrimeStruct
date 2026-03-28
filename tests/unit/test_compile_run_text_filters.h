e7830b9c6438c6724ff1ef82f470bff6dcab2f4c
Christoffer Green <christoffer.green@arm.com>
2026-03-28 11:26:17 +0100

    Fix text filter suite registration
    Restore the compile-run text filter doctest suite wrapper.
    
    Keep backend suite registration aligned with CMake.

--- tests/unit/test_compile_run_text_filters.h
+++ tests/unit/test_compile_run_text_filters.h
@@ -1,3 +1,7 @@
+TEST_SUITE_BEGIN("primestruct.compile.run.text_filters");
+
 #include "test_compile_run_text_filters_core.h"
 #include "test_compile_run_text_filters_dumps_and_if.h"
 #include "test_compile_run_text_filters_misc.h"
+
+TEST_SUITE_END();
