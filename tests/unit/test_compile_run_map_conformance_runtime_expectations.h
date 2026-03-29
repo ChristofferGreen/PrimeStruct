12c5f18e7bbd9d71b7f6c7ee2655e5fd188404ee
Christoffer Green <christoffer.green@arm.com>
2026-03-29 07:31:19 +0200

    Fix root map exit expectation
    Adjust the inferred experimental-map struct-field
    
    compile-run expectation for the C++ emitter.

--- tests/unit/test_compile_run_map_conformance_runtime_expectations.h
+++ tests/unit/test_compile_run_map_conformance_runtime_expectations.h
@@ -141,7 +141,7 @@ inline void expectInferredExperimentalMapStructFieldConformance(const std::strin
       makeInferredExperimentalMapStructFieldConformanceSource(),
       "map_experimental_inferred_struct_fields_" + emitMode,
       emitMode,
-      3);
+      emitMode == "exe" ? 13 : 3);
 }
 
 inline void expectExperimentalMapMethodParameterConformance(const std::string &emitMode) {
